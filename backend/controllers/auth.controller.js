e4d32d9b8e3b8b56f4d6002b41fe421c4e1c8438
Kristin <kristindyulgeryan@abv.bg>
2025-09-19 15:17:45 +0300

    Implement signup controller.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,18 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-  res.send("Sign up route called");
+  const { email, password, name } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const user = await User.create({ email, password, name });
+
+  res.status(201).json({ user, message: "User created succesfully" });
 };
+
 export const login = async (req, res) => {
   res.send("Login route called");
 };
