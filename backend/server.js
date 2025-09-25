8f2bb90c7c7562ac2072a0cce0c3a9917926f4ff
Kristin <kristindyulgeryan@abv.bg>
2025-09-25 16:06:24 +0300

    Add protected cart routes for managing user cart operations

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json()); // allow to parse the body of the request
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
