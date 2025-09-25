882476141c1d2057d43c9553a54ce23bbcf7f6be
Kristin <kristindyulgeryan@abv.bg>
2025-09-25 17:17:40 +0300

    Implement getCartProducts controller to return detailed cart items with quantity.

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   addToCart,
+  getCartProducts,
   removeAllFromCart,
+  updateQuantity,
 } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
