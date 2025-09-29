f0151e9c943abb1cf35c36ff7e173305b8b46035
Kristin <kristindyulgeryan@abv.bg>
2025-09-29 11:32:21 +0300

    Implement coupon routes and controller with validation and expiration handling.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.routes.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json()); // allow to parse the body of the request
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
