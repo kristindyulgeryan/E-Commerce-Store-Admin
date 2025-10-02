484e94511d421db692436d79daf51919049a2577
Kristin <kristindyulgeryan@abv.bg>
2025-10-02 12:31:16 +0300

    add radial background gradient for homepage and icon for tab bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,28 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-[#1C1A17] text-white relative overflow-hidden">
+      {/* background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div
+            className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full"
+            style={{
+              background: `radial-gradient(
+  ellipse at top,
+   rgba(255, 238, 180, 0.3) 0%,   
+  rgba(255, 215, 130, 0.2) 45%,  
+  rgba(90, 60, 30, 0.1) 100%     
+)`,
+            }}
+          />
+        </div>
+      </div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
