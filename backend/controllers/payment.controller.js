20971f6dee1d5021bba3b8d16b7ced9da7b932a7
Kristin <kristindyulgeryan@abv.bg>
2025-10-28 13:50:02 +0200

    Delete existing user coupon before creating a new one.

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -140,11 +140,12 @@ async function createStripeCoupon(discountPercentage) {
 }
 
 async function createNewCoupon(userId) {
+  await Coupon.findOneAndDelete({ userId });
   const newCoupon = new Coupon({
     code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
     discountPercentage: 10,
     expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-    userId: userId,
+    userId,
   });
 
   await newCoupon.save();
