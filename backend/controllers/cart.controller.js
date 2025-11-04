5124eaccddbbd2f31da74998284098e2a40fc32f
Kristin <kristindyulgeryan@abv.bg>
2025-11-04 13:44:03 +0200

    Update

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,34 +1,19 @@
 import Product from "../models/product.model.js";
 
-// export const getCartProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find({ _id: { $in: req.user.cartItems } });
-
-//     // Add quantity for each product
-//     const cartItems = products.map((product) => {
-//       const item = req.user.cartItems.find(
-//         (cartItem) => cartItem.id === product.id
-//       );
-//       return { ...product.toJSON(), quantity: item.quantity };
-//     });
-//     res.json(cartItems);
-//   } catch (error) {
-//     console.log("Error in getCartProducts controller", error.message);
-//     res.status(500).json({ message: "Server error", error: error.message });
-//   }
-// };
-
 export const getCartProducts = async (req, res) => {
   try {
+    const validCartItems = req.user.cartItems.filter(
+      (item) => item && item.product
+    );
     // Extract product IDs from cart items
-    const productIds = req.user.cartItems.map((item) => item.product);
+    const productIds = validCartItems.map((item) => item.product);
 
     // Fetch product details from MongoDB
     const products = await Product.find({ _id: { $in: productIds } }).lean();
 
     // Map products to include quantity
     const cartItems = products.map((product) => {
-      const item = req.user.cartItems.find(
+      const item = validCartItems.find(
         (cartItem) => cartItem.product.toString() === product._id.toString()
       );
       return { ...product, quantity: item?.quantity || 1 };
@@ -73,7 +58,7 @@ export const removeAllFromCart = async (req, res) => {
       user.cartItems = [];
     } else {
       user.cartItems = user.cartItems.filter(
-        (item) => item.product.toString() !== productId
+        (item) => item.product && item.product.toString() !== productId
       );
     }
 
@@ -90,13 +75,13 @@ export const updateQuantity = async (req, res) => {
     const { quantity } = req.body;
     const user = req.user;
     const existingItem = user.cartItems.find(
-      (item) => item.product.toString() === productId
+      (item) => item.product && item.product.toString() === productId
     );
 
     if (existingItem) {
       if (quantity === 0) {
         user.cartItems = user.cartItems.filter(
-          (item) => item.product.toString() !== productId
+          (item) => !item.product || item.product.toString() !== productId
         );
         await user.save();
         return res.json(user.cartItems);
